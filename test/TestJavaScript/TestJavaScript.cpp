71999ba3bf5cdef31139f4177fd58c68819d8dbb
Sebastian Rahner <sebastian.rahner@hicknhack-software.com>
2017-03-09 15:53:12 +0100

    * fixed PreparedJavascriptBuilder related tests

--- test/TestJavaScript/TestJavaScript.cpp
+++ test/TestJavaScript/TestJavaScript.cpp
@@ -43,31 +43,32 @@ void TestJavaScript::testInterpolation_data()
     QTest::addColumn< QString >( "templateText" );
     QTest::addColumn< QString >( "javaScriptText" );
 
-    const static QString newLine("_template.newLine();\n");
+    const static QString s_newLine("_template.newLine(%1);\n");
     const static QString s_output("_template.append(\"%1\", %2);\n");
     const static QString s_indentPart("_template.indentPart(\"%1\", %2);\n");
-    const static QString s_expression("_template.pushPartIndent(%2);_template.append(%1, %2);_template.popPartIndent();\n");
+    const static QString s_expression("_template.pushPartIndent(%2);_template.appendExpression(%1, %2);_template.popPartIndent();\n");
 
+    static auto newLine = [] (int originIndex) { return s_newLine.arg(originIndex); };
     static auto output = [](QString text, int originIndex){ return s_output.arg(text).arg(originIndex); };
     static auto indentPart = [](QString text, int originIndex){ return s_indentPart.arg(text).arg(originIndex); };
     static auto expression = [](QString text, int originIndex){ return s_expression.arg(text).arg(originIndex); };
 
-    QTest::newRow("empty") << "|" << indentPart("", 0) + newLine;
-    QTest::newRow("simple") << "|simple " << indentPart("", 0) + output("simple ", 1) + newLine;
-    QTest::newRow("indentation space") << "\t| simple" << indentPart(" ", 0) + output("simple", 1) + newLine;
-    QTest::newRow("indentation tab") << " |\tsimple" << indentPart("\t", 0) + output("simple", 1) + newLine;
+    QTest::newRow("empty") << "|" << indentPart("", 0) + newLine(1);
+    QTest::newRow("simple") << "|simple " << indentPart("", 0) + output("simple ", 1) + newLine(2);
+    QTest::newRow("indentation space") << "\t| simple" << indentPart(" ", 0) + output("simple", 1) + newLine(2);
+    QTest::newRow("indentation tab") << " |\tsimple" << indentPart("\t", 0) + output("simple", 1) + newLine(2);
 
-    QTest::newRow("no expr") << "|#(test" << indentPart("", 0) + output("#(test", 1) + newLine;
-    QTest::newRow("escaped expr") << "|##{test" << indentPart("", 0) + output("#", 1) + output("{test", 2) + newLine;
-    QTest::newRow("single expr 1") << "|#{test}" << indentPart("", 0) + expression("test", 1) + newLine;
-    QTest::newRow("single expr 2") << "|#{test}}" << indentPart("", 0) + expression("test", 1) + output("}", 2) + newLine;
-    QTest::newRow("inline expr 1") << "|simple #{test.prop} expr" << indentPart("", 0) + output("simple ", 1) + expression("test.prop", 2) + output(" expr", 3) + newLine;
-    QTest::newRow("logic expr") << "|#{ (i==0 ? ':' : \",\") }" << indentPart("", 0) + expression(" (i==0 ? ':' : \",\") ", 1) + newLine;
-    QTest::newRow("escaped expr 1") << "|escape ##{test} expr" << indentPart("", 0) + output("escape #", 1) + output("{test} expr", 2) + newLine;
-    QTest::newRow("escaped expr 2") << "|###{test}" << indentPart("", 0) + output("#", 1) + expression("test", 2) + newLine;
-    QTest::newRow("string expr 1") << "|#{\"}\"}" << indentPart("", 0) + expression("\"}\"", 1) + newLine;
-    QTest::newRow("string expr 2") << "|#{'\\'\\\"}'}" << indentPart("", 0) + expression("'\\'\\\"}'", 1) + newLine;
-    QTest::newRow("nested expr") << "|#{'#{hello}'}" << indentPart("", 0) + expression("'#{hello}'", 1) + newLine;
+    QTest::newRow("no expr") << "|#(test" << indentPart("", 0) + output("#(test", 1) + newLine(2);
+    QTest::newRow("escaped expr") << "|##{test" << indentPart("", 0) + output("#", 1) + output("{test", 2) + newLine(3);
+    QTest::newRow("single expr 1") << "|#{test}" << indentPart("", 0) + expression("test", 1) + newLine(2);
+    QTest::newRow("single expr 2") << "|#{test}}" << indentPart("", 0) + expression("test", 1) + output("}", 2) + newLine(3);
+    QTest::newRow("inline expr 1") << "|simple #{test.prop} expr" << indentPart("", 0) + output("simple ", 1) + expression("test.prop", 2) + output(" expr", 3) + newLine(4);
+    QTest::newRow("logic expr") << "|#{ (i==0 ? ':' : \",\") }" << indentPart("", 0) + expression(" (i==0 ? ':' : \",\") ", 1) + newLine(2);
+    QTest::newRow("escaped expr 1") << "|escape ##{test} expr" << indentPart("", 0) + output("escape #", 1) + output("{test} expr", 2) + newLine(3);
+    QTest::newRow("escaped expr 2") << "|###{test}" << indentPart("", 0) + output("#", 1) + expression("test", 2) + newLine(3);
+    QTest::newRow("string expr 1") << "|#{\"}\"}" << indentPart("", 0) + expression("\"}\"", 1) + newLine(2);
+    QTest::newRow("string expr 2") << "|#{'\\'\\\"}'}" << indentPart("", 0) + expression("'\\'\\\"}'", 1) + newLine(2);
+    QTest::newRow("nested expr") << "|#{'#{hello}'}" << indentPart("", 0) + expression("'#{hello}'", 1) + newLine(2);
 }
 
 void TestJavaScript::testInterpolation()
